70d2be681e4f1bf2ec3ddd8a4b414e209e5a40cd
Prashant Khatal <prashant.khatal21@gmail.com>
2017-05-08 23:42:57 +0530

    First redux

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,37 @@
-Console.log("react-redux");
+import {createStore} from "redux";
+
+const reducer = (state, action) => {
+    switch (action.type){
+        case 'ADD':
+            state = state + action.value;
+            break;
+        case 'SUBTRACT':
+            state = state + action.value;
+            break;
+    }
+
+    return state;
+};
+
+let store = createStore(reducer, 1 );
+
+//Executed once the state changes
+store.subscribe(() =>{
+    console.log('Yes Store Updated', store.getState());
+});
+
+
+store.dispatch({
+    type:'ADD',
+    value:5
+});
+
+store.dispatch({
+    type:'ADD',
+    value:45
+});
+
+store.dispatch({
+    type:'SUBTRACT',
+    value:27
+});
