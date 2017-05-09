928e9c4d555f6184ab922acc9425aa723b46e506
Prashant Khatal <prashant.khatal21@gmail.com>
2017-05-09 22:06:51 +0530

    Multiple reducers

--- src/app/index.js
+++ src/app/index.js
@@ -1,23 +1,53 @@
-import {createStore} from "redux";
+import {createStore, combineReducers} from "redux";
 
-const reducer = (state, action) => {
+const MathReducer = (state = {result: 0, lastValues: []}, action) => {
     switch (action.type){
         case 'ADD':
-            state = state + action.value;
+            state = {
+                ...state,
+                result:state.result + action.value,
+                lastValues: [...state.lastValues, action.value]
+            }
             break;
         case 'SUBTRACT':
-            state = state + action.value;
+            state = {
+                ...state,
+                result: state.result - action.value,
+                lastValues: [...state.lastValues, action.value]
+            }
             break;
     }
 
     return state;
 };
 
-let store = createStore(reducer, 1 );
+
+const UserReducer = (state = {name:'Prashant', age:27}, action) =>{
+    switch (action.type){
+        case 'SET_NAME':
+            state = {
+                ...state,
+                name: action.value
+            };
+            break;
+
+        case 'SET_AGE':
+            state = {
+                ...state,
+                age: action.value
+            };
+            break;
+    }
+
+    return state;
+}
+
+
+let store = createStore(combineReducers({UserReducer, MathReducer}));
 
 //Executed once the state changes
 store.subscribe(() =>{
-    console.log('Yes Store Updated', store.getState());
+    console.log('Yes Store Updated and new states are: ', store.getState());
 });
 
 
@@ -34,4 +64,14 @@ store.dispatch({
 store.dispatch({
     type:'SUBTRACT',
     value:27
+});
+
+store.dispatch({
+    type:'SET_NAME',
+    value:'Sachin'
+});
+
+store.dispatch({
+    type:'SET_AGE',
+    value:31
 });
